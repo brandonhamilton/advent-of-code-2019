fe06ecb4781ea35e721fdc516ed213c06e8b8464
Brandon Hamilton <brandon.hamilton@gmail.com>
2019-12-04 13:11:41 +0200

    Day 2

--- src/Solutions.hs
+++ src/Solutions.hs
@@ -17,3 +17,35 @@ day1 = do
     recFuel :: Integer -> Integer
     recFuel x | fuel x < 0 = 0
     recFuel x = let fx = fuel x in fx + recFuel fx
+
+day2 :: IO ()
+day2 =  do
+  input <- T.splitOn "," <$> readFileText "./input/day2.txt" -- Read input lines
+  let code = fromRight 0 . readEither <$> input -- Convert to Integers
+      Just result = runWith 12 2 code -- Interpret program (Part 1)
+  putTextLn $ show result -- Show result
+  let (noun, verb) = runUntil 0 0 19690720 code -- Brute force search for values (Part 2)
+  putTextLn $ show (100 * noun + verb) -- Show result
+  where
+    runUntil :: Integer -> Integer -> Integer -> [Integer] -> (Integer, Integer)
+    runUntil noun verb result code | runWith noun verb code == Just result = (noun, verb)
+    runUntil noun verb result code | verb < 99  = runUntil noun      (verb + 1) result code
+    runUntil noun verb result code | verb == 99 = runUntil (noun + 1) 0         result code
+    runUntil noun verb result code | noun < 99  = runUntil (noun + 1) verb      result code
+    runUntil noun verb _ _ = error $ "(" <> show noun <> "," <> show verb <> ")"
+
+    runWith :: Integer -> Integer -> [Integer] -> Maybe Integer
+    runWith noun verb code = let c = replace 1 noun (replace 2 verb code) in viaNonEmpty head (run c c)
+
+    replace :: Integer -> Integer -> [Integer] -> [Integer]
+    replace pos val l = l & ix (fromIntegral pos) .~ val
+
+    op :: (Integer -> Integer -> Integer) -> [Integer] -> Integer -> Integer -> Integer
+    op f l idx1 idx2 = let get idx = (l ^?! ix (fromIntegral idx)) in f (get idx1) (get idx2)
+
+    run :: [Integer] -> [Integer] -> [Integer]
+    run (99 : cont) code = code
+    run (c : i1 : i2 : o : cont) code | c == 1 || c == 2
+      = let next = replace o (op (if c == 1 then (+) else (*)) code i1 i2) code
+          in run (drop (length code - length cont) next) next
+    run (c : cont) code = error $ "Unknown opcode " <> show c <> " at position " <> show (length code - length cont)
--- test/Spec.hs
+++ test/Spec.hs
@@ -6,3 +6,4 @@ main :: IO ()
 main = do
   putTextLn "\nAdvent of Code 2019"
   putTextLn "\nDay 1" >> day1
+  putTextLn "\nDay 2" >> day2
