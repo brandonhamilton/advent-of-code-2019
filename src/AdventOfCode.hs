8c15710d070f453f388ff44a43e74aa977ddf9d8
Brandon Hamilton <brandon.hamilton@gmail.com>
2019-12-05 14:23:51 +0200

    Day 3

--- src/AdventOfCode.hs
+++ src/AdventOfCode.hs
@@ -1,9 +1,13 @@
-module Solutions where
+module AdventOfCode where
 
 import Relude
+import Data.List (minimum)
 import Lens.Micro (ix, (.~), (^?!))
 import qualified Data.Text as T
 
+--
+-- | Day 1
+--
 day1 :: IO ()
 day1 = do
   input <- lines <$> readFileText "./input/day1.txt" -- Read input lines
@@ -18,8 +22,11 @@ day1 = do
     recFuel x | fuel x < 0 = 0
     recFuel x = let fx = fuel x in fx + recFuel fx
 
+--
+-- | Day 2
+--
 day2 :: IO ()
-day2 =  do
+day2 = do
   input <- T.splitOn "," <$> readFileText "./input/day2.txt" -- Read input lines
   let code = fromRight 0 . readEither <$> input -- Convert to Integers
       Just result = runWith 12 2 code -- Interpret program (Part 1)
@@ -49,3 +56,41 @@ day2 =  do
       = let next = replace o (op (if c == 1 then (+) else (*)) code i1 i2) code
           in run (drop (length code - length cont) next) next
     run (c : cont) code = error $ "Unknown opcode " <> show c <> " at position " <> show (length code - length cont)
+
+--
+-- | Day 3
+--
+day3 :: IO ()
+day3 = do
+  input <- lines <$> readFileText "./input/day3.txt" -- Read input lines
+  let segments = foldl' segment [] . T.splitOn "," <$> input -- Extract all line segments
+      intersections = catMaybes . (intersection <$>) . sequence $ segments -- Find intersections of all wires
+  putTextLn . show $ minimum (snd <$> intersections) -- Find the minimal intersection by distance
+  putTextLn . show $ minimum (fst <$> intersections) -- Find the minimal intersection by steps
+  where
+    op :: Text -> ([Integer], Integer, Integer) -> ([Integer], Integer, Integer, Integer, Integer)
+    op t = let l = fromRight 0 (readEither (T.drop 1 t))
+            in case T.take 1 t of
+                "U" -> \(s, x, y) -> (l : s, x, y, x, y + l)
+                "D" -> \(s, x, y) -> (l : s, x, y, x, y - l)
+                "L" -> \(s, x, y) -> (l : s, x, y, x - l, y)
+                "R" -> \(s, x, y) -> (l : s, x, y, x + l, y)
+                _ -> error $ "Invalid direction: " <> t
+
+    segment :: [([Integer], Integer, Integer, Integer, Integer)] -> Text -> [([Integer], Integer, Integer, Integer, Integer)]
+    segment [] dir = [op dir ([], 0, 0)]
+    segment l@((s, _, _, x, y):_) dir = op dir (s, x, y) : l
+
+    intersection :: [([Integer], Integer, Integer, Integer, Integer)] -> Maybe (Integer, Integer)
+    intersection l@[(f:fs, fx1, fy1, fx2, fy2), (s:ss, sx1, sy1, sx2, sy2)]
+      | fx1 == fx2 && sy1 == sy2 && -- (Vertical, Horizontal)
+        fx1 >= min sx1 sx2 && fx1 <= max sx1 sx2 &&
+        sy1 >= min fy1 fy2 && sy1 <= max fy1 fy2 &&
+        abs fx1 + abs sy1 > 0
+          = Just (abs (fy1 - sy1) + sum fs + abs (sx1 - fx1) + sum ss, abs fx1 + abs sy1)
+      | fy1 == fy2 && sx1 == sx2 && -- (Horizontal, Vertical)
+        fy1 >= min sy1 sy2 && fy1 <= max sy1 sy2 &&
+        sx1 >= min fx1 fx2 && sx1 <= max fx1 fx2 &&
+        abs fy1 + abs sx1 > 0
+          = Just (abs (fx1 - sx1) + sum fs + abs (sy1 - fy1) + sum ss, abs fy1 + abs sx1)
+    intersection _ = Nothing -- No intersection
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,10 @@
 import Relude
 
-import Solutions
+import AdventOfCode
 
 main :: IO ()
 main = do
   putTextLn "\nAdvent of Code 2019"
   putTextLn "\nDay 1" >> day1
   putTextLn "\nDay 2" >> day2
+  putTextLn "\nDay 3" >> day3
