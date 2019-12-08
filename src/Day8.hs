4222e6bd662fabb557678cc39ac41d12c07bec22
Brandon Hamilton <brandon.hamilton@gmail.com>
2019-12-08 14:32:24 +0200

    Minor refactoring

--- src/Day8.hs
+++ src/Day8.hs
@@ -9,10 +9,9 @@ import qualified Data.Text as T
 
 day8 :: IO ()
 day8 = do
-  dat <- readFileText "./input/day8.txt" -- Read input data
-  let layers = T.chunksOf (25 * 6) dat -- Split into layers
-      chars = (T.length .) . T.filter . (==) -- Count chars in layer
-      checksum = L.minimumBy (compare `on` chars '0') layers -- Find layer with fewest '0' digits
+  layers <- T.chunksOf (25 * 6) <$> readFileText "./input/day8.txt" -- Read input data
+  let chars = (T.length .) . T.filter . (==) -- Count chars in layer
+  let checksum = L.minimumBy (compare `on` chars '0') layers -- Find layer with fewest '0' digits
   print $ chars '1' checksum * chars '2' checksum -- Calculate checksum
   let image = L.foldr1 (T.zipWith (\t b -> if t == '2' then b else t)) layers -- Merge layers into image
   mapM_ putTextLn (T.chunksOf 25 (T.map (\c -> if c == '1' then '█' else ' ') image)) -- Render image
